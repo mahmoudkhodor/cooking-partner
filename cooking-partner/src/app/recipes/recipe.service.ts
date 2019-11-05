039d515170b3a74ca4cd85842e9e1af9867624ae
Mahmoud Khodor <mahmoud.khodor@hotmail.com>
2019-11-05 17:07:29 +0200

    feat:  passed data using shopping service

--- cooking-partner/src/app/recipes/recipe.service.ts
+++ cooking-partner/src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe-list/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -8,16 +10,22 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'this is a simple test',
-      'https://storage.needpix.com/rsynced_images/dishes-1175495_1280.jpg'
+      'https://storage.needpix.com/rsynced_images/dishes-1175495_1280.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A test recipe2',
       'this is a simple test',
-      'https://storage.needpix.com/rsynced_images/dishes-1175495_1280.jpg'
+      'https://storage.needpix.com/rsynced_images/dishes-1175495_1280.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     )
   ];
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
